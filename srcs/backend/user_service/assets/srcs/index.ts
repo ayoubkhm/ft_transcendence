4b91bad448e8a152a94b04ae3f6d3426f9fa2a9d
root <root@Mims-LAPTOP.localdomain>
2025-06-23 23:33:29 +0200

    user/query

--- srcs/backend/user_service/assets/srcs/index.ts
+++ srcs/backend/user_service/assets/srcs/index.ts
@@ -1,13 +1,18 @@
 import fastify from 'fastify'
 import cookiesPlugin from '@fastify/cookie';
 import private_userRoutes from './routes/private_data';
+import fastifyPostgres from '@fastify/postgres';
 
 const server = fastify();
 
+server.register(fastifyPostgres, {
+  connectionString: process.env.DATABASE_URL || 'postgres://user:password@localhost:5432/mydb',
+});
 server.register(cookiesPlugin, {});
 server.register(private_userRoutes, {
   prefix: '/api/user/',
 });
+
 
 server.listen({ port: 3001, host: '0.0.0.0' }, (err, address) => {
   if (err) {
