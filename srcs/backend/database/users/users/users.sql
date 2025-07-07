7af1c0a5558963a131d69e6ccaab24b72a692141
octoross-42 <codingcoeane@gmail.com>
2025-07-08 00:46:04 +0200

    feat(Database-users): Added 2fa secrets
    Can now create 2fa_user with new_2fa_user, and added active value to table users, as long as 2fa account hasnt had its 2fa validation with validate_2fa, user account will not be active (cannot login, etc), and can rm 2fa value with rm_2fa

--- srcs/backend/database/users/users/users.sql
+++ srcs/backend/database/users/users/users.sql
@@ -5,9 +5,11 @@ CREATE TABLE IF NOT EXISTS users (
 	admin BOOLEAN NOT NULL DEFAULT FALSE,
 	email TEXT UNIQUE DEFAULT NULL,		-- nullable pour guest
 	password TEXT DEFAULT NULL,			-- nullable pour guest
-	created_at TIMESTAMP DEFAULT NOW(),
-	twofa BOOLEAN NOT NULL DEFAULT FALSE,
-	online BOOLEAN NOT NULL DEFAULT TRUE
+	created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	online BOOLEAN NOT NULL DEFAULT TRUE,
+	twofa_secret TEXT DEFAULT NULL,		-- nullable pour compte pas 2fa
+	twofa_validated BOOLEAN DEFAULT NULL,
+	active BOOLEAN NOT NULL DEFAULT TRUE		-- nullable pour 2fa pas validated
 );
 
 
@@ -23,6 +25,14 @@ BEGIN
 		END IF;
 	ELSIF NEW.type = 'guest' AND (NEW.email IS NOT NULL OR NEW.password IS NOT NULL) THEN
 		RAISE EXCEPTION 'Guest users can''t have email or password';
+	ELSIF NEW.type = 'guest' AND (NEW.twofa_secret IS NOT NULL) THEN
+		RAISE EXCEPTION '2fa accounts can''t be guest';
+	END IF;
+	
+	IF (NEW.twofa_secret IS NULL AND NEW.twofa_validated IS NOT NULL) THEN
+		RAISE EXCEPTION '2fa account mismatch: 2fa secret null and 2fa_validated not null';
+	ELSIF (NEW.twofa_secret IS NOT NULL AND NEW.twofa_validated IS NULL) THEN
+		RAISE EXCEPTION '2fa account mismatch: 2fa secret not null and 2fa_validated null';
 	END IF;
   RETURN NEW;
 END;
@@ -30,6 +40,7 @@ $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER enforce_user_constraints
 BEFORE INSERT OR UPDATE ON users
-FOR EACH ROW EXECUTE FUNCTION enforce_user_constraints();
+FOR EACH ROW
+EXECUTE FUNCTION enforce_user_constraints();
 
 GRANT ALL PRIVILEGES ON TABLE users TO ${DB_USER};
