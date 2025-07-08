469b9297c499044f9ac3507549cc639724c231ac
octoross <codingoceane@gmail.com>
2025-07-08 19:11:59 +0200

    Feat(database tournaments): added start_tournament and delete_tournament

--- srcs/backend/database/games/tournaments/tournament_CRUD.sql
+++ srcs/backend/database/games/tournaments/tournament_CRUD.sql
@@ -24,3 +24,58 @@ EXCEPTION
 		RETURN QUERY SELECT FALSE, SQLERRM;
 END;
 $$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION start_tournament(_name TEXT)
+RETURNS TABLE(success BOOLEAN, msg TEXT) AS $$
+DECLARE
+	_nbr_players INTEGER;
+	_min_players INTEGER;
+BEGIN
+	SELECT nbr_players, min_players INTO _nbr_players, _min_players
+	FROM tournaments
+	WHERE name = _name;
+	IF NOT FOUND THEN
+		RETURN QUERY SELECT FALSE, 'Tournament with this name not found';
+		RETURN ;
+	END IF;
+	IF (_min_players > _nbr_players) OR (_nbr_players < 2) THEN
+		RETURN QUERY SELECT FALSE, 'Tournament doesn''t have enough players to start';
+		RETURN ;
+	END IF;
+
+	UPDATE tournaments
+	SET state = 'RUNNING',
+		round = 1,
+		total_rounds = CEIL(LOG(2, nbr_players))
+	WHERE name = _name;
+
+	PERFORM pair_tournament(_name);
+
+	RETURN QUERY SELECT TRUE, 'Tournament started !';
+EXCEPTION
+	WHEN OTHERS THEN
+    	RETURN QUERY SELECT FALSE, SQLERRM;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION delete_tournament(_name TEXT)
+RETURNS TABLE(success BOOLEAN, msg TEXT) AS $$
+DECLARE
+	tournament_deleted BOOLEAN := FALSE;
+BEGIN
+	DELETE FROM tournaments WHERE name = _name RETURNING TRUE INTO tournament_deleted;
+	IF tournament_deleted THEN
+		RETURN QUERY SELECT TRUE, 'Tournament deleted successfully';
+	ELSE
+		RETURN QUERY SELECT FALSE, 'No tournament found with that name (delete fail)';
+	END IF;
+EXCEPTION
+	WHEN OTHERS THEN
+    	RETURN QUERY SELECT FALSE, SQLERRM;
+END;
+$$ LANGUAGE plpgsql;
+
