34a71dbe544eee1e1d67e1f47021126bde36bb3c
octoross-42 <codingoceane@gmail.com>
2025-07-14 15:41:36 +0200

    fix(Database-tournaments): Fix pairing (only to init tournament), next_pairing to do

--- srcs/backend/database/games/tournaments/pairing.sql
+++ srcs/backend/database/games/tournaments/pairing.sql
@@ -1,49 +1,40 @@
 CREATE OR REPLACE FUNCTION pair_tournament (
-	_name TEXT DEFAULT 'tournament'
+	_id INTEGER,
+	players_id INTEGER[]
 )
-RETURNS TABLE(success BOOLEAN, msg TEXT, games_id INTEGER[]) AS $$
+RETURNS INTEGER[] AS $$
 DECLARE
-	_total_rounds INTEGER NOT NULL DEFAULT 1;
-	_id INTEGER;
-	_round INTEGER;
-	players_id INTEGER[];
 	rplayers_id INTEGER[];
 	brackets_games_id INTEGER[];
 	bracket_game_id INTEGER;
 	i INTEGER;
 BEGIN
-	IF _name IS NULL THEN
-		RETURN QUERY SELECT FALSE, 'Pls specify a tournament name not null', '{}'::INTEGER[];
-		RETURN ;
-	END IF;
-	
-	SELECT id, round INTO _id, _round FROM tournaments WHERE name = _name;
-	IF NOT FOUND THEN
-		RETURN QUERY SELECT FALSE, 'No tournament found to this name', '{}'::INTEGER[];
-		RETURN ;
+	IF _id IS NULL THEN
+		RAISE EXCEPTION 'Pls specify a tournament id not null';
 	END IF;
 
-	SELECT ARRAY_AGG(player_id)
-	INTO players_id
-	FROM tournaments_players
-	WHERE tournament_id = _id;
-
-	IF array_length(players_id, 1) IS NULL OR array_length(players_id, 1) < 2 THEN
-    	RETURN QUERY SELECT FALSE, 'Not enough players in this tournament', '{}'::INTEGER[];
-		RETURN ;
+	IF EXISTS (
+    	SELECT 1 FROM games
+    	WHERE tournament_id = _id
+			AND tournament_round = 1
+			AND state != 'WAITING'
+  	) THEN
+    	RAISE EXCEPTION 'Some games have already started: can'' pair tournament';
 	END IF;
+
+	DELETE FROM games
+	WHERE tournament_id = _id
+		AND tournament_round = 1;
+
 
 	SELECT ARRAY_AGG(player_id ORDER BY random())
 	INTO rplayers_id
 	FROM unnest(players_id) AS player_id;
 
-	RAISE NOTICE 'Players order: %', rplayers_id;
-
 	i := 1;
 	WHILE i < array_length(rplayers_id, 1) LOOP
-		RAISE NOTICE 'LOOP 1: % % %', i, rplayers_id[i], rplayers_id[i + 1];
     	INSERT INTO games (p1_id, p2_id, tournament_id, tournament_round, type)
-    	VALUES (rplayers_id[i], rplayers_id[i + 1], _id, _round, 'TOURNAMENT')
+    	VALUES (rplayers_id[i], rplayers_id[i + 1], _id, 1, 'TOURNAMENT')
     	RETURNING id INTO bracket_game_id;
 
     	brackets_games_id := array_append(brackets_games_id, bracket_game_id);
@@ -52,72 +43,18 @@ BEGIN
 
 	IF array_length(rplayers_id, 1) % 2 = 1 THEN
 		INSERT INTO games (p1_id, p2_bot, tournament_id, tournament_round, type)
-    	VALUES (rplayers_id[array_length(rplayers_id, 1)], TRUE, _id, _round, 'TOURNAMENT')
+    	VALUES (rplayers_id[array_length(rplayers_id, 1)], TRUE, _id, 1, 'TOURNAMENT')
     	RETURNING id INTO bracket_game_id;
 
     	brackets_games_id := array_append(brackets_games_id, bracket_game_id);
 	END IF;
 
 	UPDATE tournaments
-	SET pairing = pairing + 1
+	SET pairing = 1
 	WHERE id = _id;
 
-	RETURN QUERY SELECT TRUE, 'Pairing created successfully', brackets_games_id;
-
-EXCEPTION
-	WHEN OTHERS THEN
-		RETURN QUERY SELECT FALSE, SQLERRM, '{}'::INTEGER[];
+	RETURN brackets_games_id;
 END;
 $$ LANGUAGE plpgsql;
 
-
-
-
-CREATE OR REPLACE FUNCTION next_round (
-	_name NAME DEFAULT 'tournament'
-)
-RETURNS TABLE(success BOOLEAN, msg TEXT) AS $$
-DECLARE
-	_id INT;
-	_total_rounds INTEGER;
-	_round INTEGER;
-	_pairing INTEGER;
-	_state tournament_state;
-	qualified INTEGER[];
-	i INTEGER;
-BEGIN
-	IF _name IS NULL THEN
-		RETURN QUERY SELECT FALSE, 'Pls specify a tournament name not null';
-		RETURN ;
-	END IF;
-	
-	SELECT id, round, total_rounds, pairing, tournament_state
-	INTO _id, _round, _total_rounds, _pairing, _state
-	FROM tournaments
-	WHERE name = _name;
-
-	IF NOT FOUND THEN
-		RETURN QUERY SELECT FALSE, 'No tournament found to this name';
-		RETURN ;
-	END IF;
-
-	IF _round >= _total_rounds AND _state != 'WAITING' THEN
-		RETURN QUERY SELECT FALSE, 'Tournament is over !';
-		RETURN ;
-	END IF;
-
-	SELECT ARRAY_AGG(player_id)
-	INTO qualified
-	FROM tournaments_players
-	WHERE tournament_id = _tournament_id
-		AND round = _round
-		AND current_round_over = TRUE
-		AND stil_in = TRUE;
-
-
-EXCEPTION
-	WHEN OTHERS THEN
-		RETURN QUERY SELECT FALSE, SQLERRM;
-END;
-$$ LANGUAGE plpgsql;
 
--- srcs/backend/database/games/tournaments/tournament_CRUD.sql
+++ srcs/backend/database/games/tournaments/tournament_CRUD.sql
@@ -26,37 +26,94 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-
-CREATE OR REPLACE FUNCTION start_tournament(_name TEXT)
-RETURNS TABLE(success BOOLEAN, msg TEXT) AS $$
+CREATE OR REPLACE FUNCTION init_tournament(_name TEXT)
+RETURNS TABLE(success BOOLEAN, msg TEXT, games_id INTEGER[]) AS $$
 DECLARE
+	_id INTEGER;
+	_round INTEGER;
 	_nbr_players INTEGER;
 	_min_players INTEGER;
+	_max_players INTEGER;
+	players_id INTEGER[];
 BEGIN
-	SELECT nbr_players, min_players INTO _nbr_players, _min_players
-	FROM tournaments
-	WHERE name = _name;
-	IF NOT FOUND THEN
-		RETURN QUERY SELECT FALSE, 'Tournament with this name not found';
+	IF _name IS NULL THEN
+		RETURN QUERY SELECT FALSE, 'Tournament name cant be null', '{}'::INTEGER[];
 		RETURN ;
 	END IF;
-	IF (_min_players > _nbr_players) OR (_nbr_players < 2) THEN
-		RETURN QUERY SELECT FALSE, 'Tournament doesn''t have enough players to start';
+
+	SELECT id, round, min_players, max_players INTO _id, _round, _min_players, _max_players
+	FROM tournaments WHERE name = _name;
+	IF NOT FOUND THEN
+		RETURN QUERY SELECT FALSE, 'No tournament found to this id', '{}'::INTEGER[];
+		RETURN ;
+	END IF;
+	
+	IF _round != 0 THEN
+		RETURN QUERY SELECT FALSE, FORMAT('Can''t init tournaments: round (%s) > 0', _round), '{}'::INTEGER[];
+		RETURN ;
+	END IF;
+
+	SELECT ARRAY_AGG(player_id)
+	INTO players_id
+	FROM tournaments_players
+	WHERE tournament_id = _id;
+
+	_nbr_players := array_length(players_id, 1);
+	_max_players := GREATEST(_max_players, 2);
+	_min_players := GREATEST(2, _min_players);
+	IF _nbr_players IS NULL OR (_nbr_players < _min_players) OR (_nbr_players > _max_players) THEN
+    	RETURN QUERY SELECT FALSE, FORMAT('Players don''t match nbr of players required (%s players, min %s, max %s)', _nbr_players, _min_players, _max_players), '{}'::INTEGER[];
 		RETURN ;
 	END IF;
 
 	UPDATE tournaments
-	SET state = 'RUNNING',
+	SET state = 'WAITING',
 		round = 1,
 		total_rounds = CEIL(LOG(2, nbr_players))
-	WHERE name = _name;
+	WHERE id = _id;
 
-	PERFORM pair_tournament(_name);
-
-	RETURN QUERY SELECT TRUE, 'Tournament started !';
+	RETURN QUERY SELECT TRUE, 'Tournament initialized !', pair_tournament(_id, players_id);
 EXCEPTION
 	WHEN OTHERS THEN
-    	RETURN QUERY SELECT FALSE, SQLERRM;
+    	RETURN QUERY SELECT FALSE, SQLERRM, '{}'::INTEGER[];
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION next_round (
+	_name NAME DEFAULT 'tournament'
+)
+RETURNS TABLE(success BOOLEAN, msg TEXT) AS $$
+DECLARE
+	_id INT;
+	_total_rounds INTEGER;
+	_round INTEGER;
+	_pairing INTEGER;
+	_state tournament_state;
+	round_games INTEGER[];
+	i INTEGER;
+BEGIN
+	IF _name IS NULL THEN
+		RETURN QUERY SELECT FALSE, 'Pls specify a tournament name not null';
+		RETURN ;
+	END IF;
+	
+	SELECT id, round, pairing, state
+	INTO _id, _round, _pairing, _state
+	FROM tournaments
+	WHERE name = _name;
+
+	IF NOT FOUND THEN
+		RETURN QUERY SELECT FALSE, 'No tournament found to this name';
+		RETURN ;
+	END IF;
+
+	
+
+EXCEPTION
+	WHEN OTHERS THEN
+		RETURN QUERY SELECT FALSE, SQLERRM;
 END;
 $$ LANGUAGE plpgsql;
 
@@ -67,11 +124,11 @@ RETURNS TABLE(success BOOLEAN, msg TEXT) AS $$
 DECLARE
 	tournament_deleted BOOLEAN := FALSE;
 BEGIN
-	DELETE FROM tournaments WHERE name = _name RETURNING TRUE INTO tournament_deleted;
+	DELETE FROM tournaments WHERE name = _name AND (state != 'RUNNING') RETURNING TRUE INTO tournament_deleted;
 	IF tournament_deleted THEN
 		RETURN QUERY SELECT TRUE, 'Tournament deleted successfully';
 	ELSE
-		RETURN QUERY SELECT FALSE, 'No tournament found with that name (delete fail)';
+		RETURN QUERY SELECT FALSE, 'Tournament either doesnt exist or is running (cant delete running tournaments) (delete fail)';
 	END IF;
 EXCEPTION
 	WHEN OTHERS THEN
