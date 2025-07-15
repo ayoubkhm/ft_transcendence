7d4dc6aab61a686ddc75b691d478a6fb2ff298d5
GinoDbm <gino.doub@gmail.com>
2025-07-15 17:32:48 +0200

    avator PRESQUE ok

--- srcs/backend/user_service/assets/srcs/index.ts
+++ srcs/backend/user_service/assets/srcs/index.ts
@@ -1,34 +1,42 @@
-import fastify from 'fastify'
+import fastify from 'fastify';
 import cookiesPlugin from '@fastify/cookie';
 import multipart from '@fastify/multipart';
+import fastifyPostgres from '@fastify/postgres';
+import fastifyStatic from '@fastify/static';
+import path from 'path';
+
 import private_userRoutes from './routes/private_data';
 import public_userRoutes from './routes/public_data';
 import friendsRoutes from './routes/friends';
-// import tournamentsRoutes from './routes/tournaments';
-import fastifyPostgres from '@fastify/postgres';
 
-// Trust proxy headers (e.g., X-Forwarded-Proto) when behind SSL termination
 const server = fastify({ trustProxy: true });
+
+const avatarsPath = path.join(__dirname, '../../public/avatars');
+console.log('Serving static avatars from:', avatarsPath);
+
+// Enregistre le plugin pour servir les fichiers statiques AVANT les routes
+server.register(fastifyStatic, {
+  root: path.join(__dirname, '../../public/avatars'), // chemin vers le dossier avatars
+  prefix: '/avatars/', // correspond à l’URL /avatars/*
+  // Optionnel : cacheControl, maxAge, etc.
+});
 
 server.register(multipart);
 server.register(fastifyPostgres, {
-  // TODO: replace value db
   connectionString: 'postgresql://transcendence:imthebest@database_service:5432/db',
 });
 server.register(cookiesPlugin, {});
-server.register(private_userRoutes, { prefix: '/api/user',});
-server.register(public_userRoutes, { prefix: '/api/user',});
-server.register(friendsRoutes, { prefix: '/api/user',});
-// Tournament dashboard endpoints removed temporarily
-// server.register(tournamentsRoutes, { prefix: '/api/tournaments' });
+
+server.register(private_userRoutes, { prefix: '/api/user' });
+server.register(public_userRoutes, { prefix: '/api/user' });
+server.register(friendsRoutes, { prefix: '/api/user' });
 
 console.log(server.printRoutes());
 
-server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) =>
-{
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-    console.log(`ready`);
-})
+server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server ready at ${address}`);
+});
