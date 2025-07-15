3b9970d3a9d2fbb4c0ed55918a52e33770ae0017
octoross-42 <codingoceane@gmail.com>
2025-07-15 03:01:24 +0200

    feat(Database-tournaments): Pairing done, now all brackets games are predefined, and games can now have p1/2_winnerof which is references to games id
    Now round 0 exists for tournaments, and these prematchs are to make sure theres a number of match that are a power of 2 in round 1, all games id are return in form of JSON: { [round: ..., matchs: ...]}

--- srcs/backend/database/games/tournaments/pairing.sql
+++ srcs/backend/database/games/tournaments/pairing.sql
@@ -1,18 +1,24 @@
 CREATE OR REPLACE FUNCTION pair_tournament (
 	_id INTEGER,
-	players_id INTEGER[]
+	players_id INTEGER[],
+	_nbr_players INTEGER
 )
-RETURNS INTEGER[] AS $$
+RETURNS JSON AS $$
 DECLARE
 	rplayers_id INTEGER[];
-	brackets_games_id INTEGER[];
-	bracket_game_id INTEGER;
 	i INTEGER;
+	_p1_id INTEGER;
+	_p2_id INTEGER;
+	_p1_winnerof INTEGER;
+	_p2_winnerof INTEGER;
+	brackets_json JSONB := '[]'::jsonb;
+	game_created RECORD;
+	roundi_games JSONB;
+	prev_round_games JSONB;
+	_round_index INTEGER;
+	nbr_players_roundi INTEGER;
+	nbr_matchs_round0 INTEGER;
 BEGIN
-	IF _id IS NULL THEN
-		RAISE EXCEPTION 'Pls specify a tournament id not null';
-	END IF;
-
 	DELETE FROM games
 	WHERE tournament_id = _id;
 
@@ -20,17 +26,75 @@ BEGIN
 	INTO rplayers_id
 	FROM unnest(players_id) AS player_id;
 
+	nbr_players_roundi := 2 ^ (FLOOR(LOG(2, _nbr_players)));
+	nbr_matchs_round0 := _nbr_players - nbr_players_roundi;
 	i := 1;
-	WHILE i < array_length(rplayers_id, 1) LOOP
-    	INSERT INTO games (p1_id, p2_id, tournament_id, tournament_round, type)
-    	VALUES (rplayers_id[i], rplayers_id[i + 1], _id, 1, 'TOURNAMENT')
-    	RETURNING id INTO bracket_game_id;
+	roundi_games := '[]'::jsonb;
+	WHILE (i <= nbr_matchs_round0) LOOP
 
-    	brackets_games_id := array_append(brackets_games_id, bracket_game_id);
-    	i := i + 2;
+    	_p1_id := rplayers_id[_nbr_players - (2 * i) + 1];
+    	_p2_id := rplayers_id[_nbr_players - (2 * i) + 2];
+		
+		INSERT INTO games (tournament_id, tournament_round, p1_id, p2_id, type)
+		VALUES (_id, 0, _p1_id, _p2_id, 'TOURNAMENT')
+		RETURNING * INTO game_created;
+		roundi_games := roundi_games || to_jsonb(game_created);
+		i := i + 1;
+	END LOOP;
+	brackets_json := brackets_json || jsonb_build_object('round', 0, 'matchs', roundi_games);
+
+
+	i := 1;
+	prev_round_games := roundi_games;
+	roundi_games := '[]'::jsonb;
+	WHILE (i <= nbr_players_roundi) LOOP
+    	IF (i <= nbr_players_roundi - nbr_matchs_round0) THEN
+			_p1_id := rplayers_id[i];
+			_p1_winnerof := NULL;
+		ELSE
+			_p1_id := NULL;
+			_p1_winnerof := ((prev_round_games -> (i - 1 - 2 * nbr_players_roundi + _nbr_players)) ->> 'id')::INTEGER;
+		END IF;
+		IF (i + 1 <= nbr_players_roundi - nbr_matchs_round0) THEN
+			_p2_id := rplayers_id[i + 1];
+			_p2_winnerof := NULL;
+		ELSE
+			_p2_id := NULL;
+			_p2_winnerof := ((prev_round_games -> (i - 2 * nbr_players_roundi + _nbr_players)) ->> 'id')::INTEGER;
+		END IF;
+    	
+		INSERT INTO games (tournament_id, tournament_round, p1_id, p2_id, type, p1_winnerof, p2_winnerof)
+		VALUES (_id, 1, _p1_id, _p2_id, 'TOURNAMENT', _p1_winnerof, _p2_winnerof)
+		RETURNING * INTO game_created;
+		
+		roundi_games := roundi_games || to_jsonb(game_created);
+
+		i := i + 2;
+	END LOOP;
+	brackets_json := brackets_json || jsonb_build_object('round', 1, 'matchs', roundi_games);
+
+	_round_index := 2;
+	nbr_players_roundi := nbr_players_roundi / 2;
+	WHILE (nbr_players_roundi > 1) LOOP
+		prev_round_games := roundi_games;
+		roundi_games := '[]'::jsonb;
+		i := 1;
+		WHILE (i < nbr_players_roundi) LOOP
+			_p1_winnerof := ((prev_round_games -> (i - 1)) ->> 'id')::INTEGER;
+			_p2_winnerof := ((prev_round_games -> i) ->> 'id')::INTEGER;
+			INSERT INTO games (tournament_id, tournament_round, type, p1_winnerof, p2_winnerof)
+			VALUES (_id, _round_index, 'TOURNAMENT', _p1_winnerof, _p2_winnerof)
+			RETURNING * INTO game_created;
+			
+			roundi_games := roundi_games || to_jsonb(game_created);
+			i := i + 2;
+		END LOOP;
+
+		brackets_json := brackets_json || jsonb_build_object('round', _round_index, 'matchs', roundi_games);
+		nbr_players_roundi := nbr_players_roundi / 2;
+		_round_index := _round_index + 1;
 	END LOOP;
 
-	RETURN brackets_games_id;
+	RETURN brackets_json;
 END;
 $$ LANGUAGE plpgsql;
-
