9c34c4b08be3b7087d285f1f3ab560f637674c80
octoross-42 <codingoceane@gmail.com>
2025-07-20 06:32:19 +0200

    feat(game-service-->-win-game): Adjust function to new db

--- srcs/backend/tournament_service/asset/srcs/game_flow.ts
+++ srcs/backend/tournament_service/asset/srcs/game_flow.ts
@@ -5,31 +5,29 @@ export default async function gameFlowRoutes(server: FastifyInstance) {
   server.post<{
     Body: {
       gameId: number;
-      winnerId: number;
-      p1_score: number;
-      p2_score: number;
+      winnerSide: boolean;
     };
   }>('/game/end', async (request, reply) => {
-    const { gameId, winnerId, p1_score, p2_score } = request.body;
+    const { gameId, winnerSide } = request.body;
     
     console.log(`[Game End] Received notification for gameId: ${gameId}`);
-    console.log(`[Game End]   Winner ID: ${winnerId}`);
-    console.log(`[Game End]   Scores: P1=${p1_score}, P2=${p2_score}`);
+    console.log(`[Game End]   Winner ID: ${winnerSide}`);
+    // console.log(`[Game End]   Scores: P1=${p1_score}, P2=${p2_score}`);
 
     const client = await server.pg.connect();
     try {
-      console.log(`[Game End] Calling end_game(${gameId}, ${winnerId}, ${p1_score}, ${p2_score}) in DB...`);
+      console.log(`[Game End] Calling win_game(${gameId}, ${winnerSide}) in DB...`);
       const res = await client.query(
-        'SELECT * FROM end_game($1::INTEGER, $2::INTEGER, $3::INTEGER, $4::INTEGER)',
-        [gameId, winnerId, p1_score, p2_score]
+        'SELECT * FROM win_game($1::INTEGER, $2::BOOLEAN)',
+        [gameId, winnerSide]
       );
 
       const result = res.rows[0];
       console.log('[Game End] Received from DB:', result);
 
       if (result.success) {
-        console.log(`[Game End] DB call successful. Broadcasting update for tournamentId: ${result.tournament_id}.`);
-        await broadcastTournamentUpdate(server, result.tournament_id, client);
+        console.log(`[Game End] DB call successful. Broadcasting update for tournamentId: ${result.tid}.`);
+        await broadcastTournamentUpdate(server, result.tid, client);
         console.log('[Game End] Broadcast complete.');
       } else {
         console.error(`[Game End] DB call failed for game ${gameId}. Reason: ${result.msg}`);
