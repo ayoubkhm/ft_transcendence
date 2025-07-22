1b2b97166a066b2ba44b13a389f6054c565af0f9
Oukicmoi <goulven.traimany@gmail.com>
2025-07-22 04:35:51 +0200

    add stats to other users

--- srcs/frontend/src/stats/setup.ts
+++ srcs/frontend/src/stats/setup.ts
@@ -1,36 +1,60 @@
 import { navigate } from "../lib/router";
+import { statsState } from './show_stats'; // si besoin
 
 export function close_stats()
 {
-	const statsContainer = document.getElementById("stats-container") as HTMLButtonElement | null;
+	const statsMatchs = document.getElementById("stats-matchs") as HTMLButtonElement | null;
 	const statsPopup = document.getElementById("stats-popup") as HTMLButtonElement | null;
 
 	if (statsPopup)
 		statsPopup.classList.add('hidden');
-	if (statsContainer)
-		statsContainer.innerHTML = '';
+	if (statsMatchs)
+		statsMatchs.innerHTML = '';
+	const winrateText = document.getElementById("stats-winrate-text");
+	const winrateCircle = document.getElementById("stats-winrate");
+	if (winrateText)
+		winrateText.textContent = 'N/A';
+	if (winrateCircle)
+		winrateCircle.setAttribute('stroke-dasharray', '0 251.2');
+
+	const blockrateText = document.getElementById("stats-blockrate-text");
+	const blockrateCircle = document.getElementById("stats-blockrate");
+	if (blockrateText)
+		blockrateText.textContent = 'N/A';
+	if (blockrateCircle)
+		blockrateCircle.setAttribute('stroke-dasharray', '0 251.2');
 }
+
 
 export default function setupUserStats(): void
 {
-    const statsPopup = document.getElementById('stats-popup');
-    const closeButton = document.getElementById('stats-close');
+	const statsPopup = document.getElementById('stats-popup');
+	const closeButton = document.getElementById('stats-close');
 
-    if (closeButton && statsPopup) {
-        closeButton.addEventListener('click', () => navigate('profile'));
+	if (closeButton && statsPopup)
+	{
+		closeButton.addEventListener('click', () => {
+			close_stats();
+			if (statsState.isMyOwnStats)
+				navigate('profile');
+		});
 
-        // Close modal on click outside
-        statsPopup.addEventListener('click', (e) => {
-            if (e.target === statsPopup) {
-                navigate('profile');
-            }
-        });
-    }
+		statsPopup.addEventListener('click', (e) => {
+			if (e.target === statsPopup)
+			{
+				close_stats();
+				if (statsState.isMyOwnStats)
+					navigate('profile');
+			}
+		});
+	}
 
-    // Close modal on Escape key
-    document.addEventListener('keydown', (e) => {
-        if (e.key === 'Escape' && statsPopup && !statsPopup.classList.contains('hidden')) {
-            navigate('profile');
-        }
-    });
+	document.addEventListener('keydown', (e) => {
+		if (e.key === 'Escape' && statsPopup && !statsPopup.classList.contains('hidden'))
+		{
+			close_stats();
+			if (statsState.isMyOwnStats)
+				navigate('profile');
+		}
+	});
 }
