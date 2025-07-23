3a4feff5de00efc45f35d0c005da9313aaec5b7f
octoross <codingoceane@gmail.com>
2025-07-23 21:27:10 +0200

    rm blockrate

--- srcs/frontend/src/stats/render/renderStats.ts
+++ srcs/frontend/src/stats/render/renderStats.ts
@@ -6,7 +6,7 @@ import renderMatchHistory from "./renderMatchsHistory";
 export default function renderStats(userId:number, data: UserStats): void
 {
     updateWinrate(data.won_count??0, data.lost_count??0);
-    updateBlockrate(data.block_count??0, data.missed_count??0);
+    // updateBlockrate(data.block_count??0, data.missed_count??0);
     if (data.matches)
         renderMatchHistory(userId, data.matches);
 }
