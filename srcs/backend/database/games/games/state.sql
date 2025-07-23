da62d0aaa9b87d3035cb5757b6f0790eecefe998
octoross <codingoceane@gmail.com>
2025-07-23 18:08:08 +0200

    Database: returns tid and tround when game over

--- srcs/backend/database/games/games/state.sql
+++ srcs/backend/database/games/games/state.sql
@@ -187,7 +187,7 @@ CREATE OR REPLACE FUNCTION win_game (
 	_p1_score INTEGER DEFAULT NULL,
 	_p2_score INTEGER DEFAULT NULL
 )
-RETURNS TABLE(success BOOLEAN, msg TEXT, tround INTEGER) AS $$
+RETURNS TABLE(success BOOLEAN, msg TEXT, tid INTEGER, tround INTEGER) AS $$
 DECLARE
 	_state game_state;
 	_p1_id INTEGER;
@@ -198,7 +198,7 @@ DECLARE
 	_tournament_id INTEGER;
 BEGIN
 	IF _id IS NULL THEN
-		RETURN QUERY SELECT FALSE, 'Pls specify a id game not null', NULL::INTEGER AS tround;
+		RETURN QUERY SELECT FALSE, 'Pls specify a id game not null', NULL::INTEGER AS tid, NULL::INTEGER AS tround;
 		RETURN ;
 	END IF;
 	
@@ -208,12 +208,12 @@ BEGIN
 	WHERE id = _id;
 
 	IF NOT FOUND THEN
-		RETURN QUERY SELECT FALSE, FORMAT('Game with id %s not found', _id), NULL::INTEGER AS tround;
+		RETURN QUERY SELECT FALSE, FORMAT('Game with id %s not found', _id), NULL::INTEGER AS tid, NULL::INTEGER AS tround;
 		RETURN ;
 	END IF;
 
 	IF _state = 'OVER' THEN
-        RETURN QUERY SELECT FALSE, 'Can''t win game that is already over', NULL::INTEGER AS tround;
+        RETURN QUERY SELECT FALSE, 'Can''t win game that is already over', NULL::INTEGER AS tid, NULL::INTEGER AS tround;
         RETURN ;
     END IF;
 
@@ -262,10 +262,10 @@ BEGIN
 		END IF;
 	END IF;
 
-	RETURN QUERY SELECT TRUE, 'Game successfully won', _tournament_round AS tround;
+	RETURN QUERY SELECT TRUE, 'Game successfully won', _tournament_id as tid, _tournament_round AS tround;
 	
 EXCEPTION
 	WHEN OTHERS THEN
-		RETURN QUERY SELECT FALSE, SQLERRM, NULL::INTEGER AS tround;
+		RETURN QUERY SELECT FALSE, SQLERRM, NULL::INTEGER AS tid, NULL::INTEGER AS tround;
 END;
 $$ LANGUAGE plpgsql;
